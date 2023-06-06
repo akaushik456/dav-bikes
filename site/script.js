e61e65ae8b3e4892576c24507a7323a8a0b0391e
akaushik456 <kaushikaman239@gmail.com>
2023-06-06 10:37:19 -0700

    add-countdown

--- site/script.js
+++ site/script.js
@@ -48,3 +48,31 @@ $('body').on('mouseenter mouseleave','.dropdown',function(e){
       $('[data-toggle="dropdown"]', _d).attr('aria-expanded',_d.is(':hover'));
     },300);
   });
+
+
+
+  const main = () => {
+    const second = 1000
+    const minute = second * 60
+    const hour = minute * 60
+    const day = hour * 24
+        
+  
+    const EVENTDATE = new Date('June 1, 2025, 19:00:00')
+   
+    const countDown = new Date(EVENTDATE).getTime()
+    const x = setInterval(() => {
+  
+          const now = new Date().getTime()
+          const distance = countDown - now
+  
+          document.getElementById("days").innerText = Math.floor(distance / day)
+          document.getElementById("hours").innerText = Math.floor((distance % day) / (hour))
+          document.getElementById("minutes").innerText = Math.floor((distance % hour) / (minute))
+          document.getElementById("seconds").innerText = Math.floor((distance % minute) / second)
+  
+        
+        }, 0)
+    }
+  
+  main();
